0bde11798bbecc3afd0a8a658505504630b83527
camilo-tecser <desarrollador30@tecsersas.com>
2025-04-21 15:34:56 -0500

    Update contact section in custodia view with form and validation messages; adjust layout and styling

--- resources/views/custodia.blade.php
+++ resources/views/custodia.blade.php
@@ -97,9 +97,11 @@
                 </ul>
             </div>
         </div>
-        <div class="row justify-content-center gx-5 ">
+        <div class="row justify-content-center gx-5">
             <div class="col-12 col-md-6 mb-4">
-                <div class="text-center blue-bootom-border title-section-cust font-weight-bold mb-4">
+                <div
+                    class="text-center blue-bootom-border title-section-cust font-weight-bold mb-4"
+                >
                     <h2>{{ __("validation.cust.importance") }}</h2>
                 </div>
                 <div class="w-90">
@@ -132,6 +134,123 @@
     <!-- .container -->
 </div>
 <!-- .page-section -->
+
+<!-- contacto -->
+<section id="contact" class="contact">
+    <div class="espacio1234">
+        <div class="container">
+            <div class="section-title">
+                <h2 class="title-section-contact" style="text-align: center">
+                    {{ __("validation.contact.title") }}
+                </h2>
+            </div>
+        </div>
+
+        <div></div>
+
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-lg-4">
+                    <div class="info">
+                        <div class="address">
+                            <i class="fa-solid fa-location-dot"></i>
+
+                            <h4>{{ __("validation.contact.direc") }}</h4>
+
+                            <p>Cl 98ª #51-37</p>
+                            <p>{{ __("validation.contact.edif") }}</p>
+                            <p>Bogotá</p>
+                        </div>
+                        <br />
+                        <div class="phone">
+                            <i class="fa-solid fa-phone-volume"></i>
+
+                            <h4>{{ __("validation.contact.phone") }}</h4>
+
+                            <p>323 6567087</p>
+                        </div>
+                        <br />
+                        <div class="email">
+                            <i class="fa-solid fa-envelope-circle-check"></i>
+
+                            <h4>{{ __("validation.contact.email") }}</h4>
+
+                            <p>info@hometicsa.com</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-8 mt-5 mt-lg-0">
+                    <form
+                        action="{{ route('mail.store') }}"
+                        method="POST"
+                        enctype="multipart/form-data"
+                    >
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    class="form-control"
+                                    id="name"
+                                    placeholder="{{
+                                        __('validation.contact.name')
+                                    }}"
+                                    required
+                                />
+                            </div>
+                            <div class="col-md-6 form-group mt-3 mt-md-0">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="phone"
+                                    id="subject"
+                                    placeholder="{{
+                                        __('validation.contact.phone')
+                                    }}"
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group mt-3">
+                            <input
+                                type="email"
+                                class="form-control"
+                                name="email"
+                                id="email"
+                                placeholder="{{
+                                    __('validation.contact.mail')
+                                }}"
+                                required
+                            />
+                        </div>
+                        <div class="form-group mt-3">
+                            <textarea
+                                class="form-control"
+                                name="message"
+                                rows="5"
+                                placeholder="{{
+                                    __('validation.contact.message')
+                                }}"
+                                required
+                            ></textarea>
+                        </div>
+
+                        <div class="text-center">
+                            <button
+                                type="submit"
+                                class="btn btn-success btn-xl mt-3"
+                            >
+                                {{ __("validation.contact.send") }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 @stop @section('css') @stop @section('js')
 <script>
